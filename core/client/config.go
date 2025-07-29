f77169ba65419d051ca7a5c6fed9a51c45e9f61f
V2BTC <admin@wikiped.me>
2025-07-29 14:59:35 +0800

    Update config.go

--- core/client/config.go
+++ core/client/config.go
@@ -27,9 +27,6 @@ type Config struct {
 	FastOpen        bool
 	DecoyURL        string
 
-	EnableUQUIC     bool
-	UQUICSpecID     quic.QUICID // 类型必须为 quic.QUICID
-
 	filled bool // whether the fields have been verified and filled
 }
 
@@ -110,6 +107,9 @@ type QUICConfig struct {
 	MaxIdleTimeout                 time.Duration
 	KeepAlivePeriod                time.Duration
 	DisablePathMTUDiscovery        bool
+
+	EnableUQUIC     bool
+	UQUICSpecID     quic.QUICID // 类型必须为 quic.QUICID
 }
 
 // BandwidthConfig describes the maximum bandwidth that the server can use, in bytes per second.
